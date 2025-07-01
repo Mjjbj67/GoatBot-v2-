e254a5d4d07610ed0c593d1ddeb07b3a4051f73a
Mjjbj67 <165726694+Mjjbj67@users.noreply.github.com>
2025-07-01 21:05:34 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "╭─❍ ᏦᎽᎾᎿᎯᏦᎯ ❍─╮";
+const doNotDelete = "╭─❍ SONIC ❍─╮";
 
 module.exports = {
   config: {
